9e1ad81ce7f95977379a6b51ae9612b68202f6c5
eduardoolimpiodev <eduardo.olimpio.developer@gmail.com>
2024-06-28 11:13:59 -0300

    Created ApplicationDbContext.

---
+++ TodoListApp.Infrastructure/DbContext/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoListApp.Domain.Entities;
+
+namespace TodoListApp.Infrastructure
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<TodoList> TodoLists { get; set; }
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<TodoList>()
+                .HasIndex(t => t.Name)
+                .IsUnique();
+        }
+    }
+}

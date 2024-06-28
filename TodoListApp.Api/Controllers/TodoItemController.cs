0a9f30370d56eabc2324af94ceed14af5410edde
eduardoolimpiodev <eduardo.olimpio.developer@gmail.com>
2024-06-28 12:27:17 -0300

    feat: Controllers created.

---
+++ TodoListApp.Api/Controllers/TodoItemController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoListApp.Application.Interfaces;
+using TodoListApp.Domain.Entities;
+
+namespace TodoListApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoItemController : ControllerBase
+    {
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoItemController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+
+        [HttpGet("list/{listId}")]
+        public async Task<IActionResult> GetAllByListId(int listId)
+        {
+            var todoItems = await _todoItemService.GetAllByListIdAsync(listId);
+            return Ok(todoItems);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var todoItem = await _todoItemService.GetByIdAsync(id);
+            if (todoItem == null) return NotFound();
+            return Ok(todoItem);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] TodoItem todoItem)
+        {
+            if (todoItem == null) return BadRequest();
+
+            await _todoItemService.CreateAsync(todoItem);
+            return CreatedAtAction(nameof(GetById), new { id = todoItem.Id }, todoItem);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] TodoItem todoItem)
+        {
+            if (id != todoItem.Id) return BadRequest();
+
+            await _todoItemService.UpdateAsync(todoItem);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _todoItemService.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
---
+++ TodoListApp.Api/Controllers/TodoListController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoListApp.Application.Interfaces;
+using TodoListApp.Domain.Entities;
+
+namespace TodoListApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoListController : ControllerBase
+    {
+        private readonly ITodoListService _todoListService;
+
+        public TodoListController(ITodoListService todoListService)
+        {
+            _todoListService = todoListService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var todoLists = await _todoListService.GetAllAsync();
+            return Ok(todoLists);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var todoList = await _todoListService.GetByIdAsync(id);
+            if (todoList == null) return NotFound();
+            return Ok(todoList);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] TodoList todoList)
+        {
+            if (todoList == null) return BadRequest();
+
+            await _todoListService.CreateAsync(todoList);
+            return CreatedAtAction(nameof(GetById), new { id = todoList.Id }, todoList);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] TodoList todoList)
+        {
+            if (id != todoList.Id) return BadRequest();
+
+            await _todoListService.UpdateAsync(todoList);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _todoListService.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}

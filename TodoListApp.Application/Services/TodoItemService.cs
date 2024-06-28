3c2870a590ab8ae11b648b4fe3454bac26aff28f
eduardoolimpiodev <eduardo.olimpio.developer@gmail.com>
2024-06-28 12:02:09 -0300

    Implementation of services created.

---
+++ TodoListApp.Application/Services/TodoItemService.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoListApp.Application.Interfaces;
+using TodoListApp.Domain.Entities;
+using TodoListApp.Infrastructure;
+
+namespace TodoListApp.Application.Services
+{
+    public class TodoItemService : ITodoItemService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TodoItemService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<TodoItem>> GetAllByListIdAsync(int listId)
+        {
+            return await _context.TodoItems.Where(t => t.TodoListId == listId).ToListAsync();
+        }
+
+        public async Task<TodoItem> GetByIdAsync(int id)
+        {
+            return await _context.TodoItems.FindAsync(id);
+        }
+
+        public async Task CreateAsync(TodoItem todoItem)
+        {
+            _context.TodoItems.Add(todoItem);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(TodoItem todoItem)
+        {
+            _context.TodoItems.Update(todoItem);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem != null)
+            {
+                _context.TodoItems.Remove(todoItem);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ TodoListApp.Application/Services/TodoListService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoListApp.Application.Interfaces;
+using TodoListApp.Domain.Entities;
+using TodoListApp.Infrastructure;
+
+namespace TodoListApp.Application.Services
+{
+    public class TodoListService : ITodoListService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TodoListService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<TodoList>> GetAllAsync()
+        {
+            return await _context.TodoLists.Include(t => t.Items).ToListAsync();
+        }
+
+        public async Task<TodoList> GetByIdAsync(int id)
+        {
+            return await _context.TodoLists.Include(t => t.Items)
+                                           .FirstOrDefaultAsync(t => t.Id == id);
+        }
+
+        public async Task CreateAsync(TodoList todoList)
+        {
+            _context.TodoLists.Add(todoList);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(TodoList todoList)
+        {
+            _context.TodoLists.Update(todoList);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var todoList = await _context.TodoLists.FindAsync(id);
+            if (todoList != null)
+            {
+                _context.TodoLists.Remove(todoList);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}

89434a12c8ce23269cebe543d1feb02b4ef88c7e
eduardoolimpiodev <eduardo.olimpio.developer@gmail.com>
2024-06-28 11:44:22 -0300

    feat: Created interfaces to manage lists and tasks.

---
+++ TodoListApp.Application/Interfaces/ITodoItemService.cs
@@ -0,0 +1,13 @@
+﻿using TodoListApp.Domain.Entities;
+
+namespace TodoListApp.Application.Interfaces
+{
+    public interface ITodoItemService
+    {
+        Task<IEnumerable<TodoItem>> GetAllByListIdAsync(int listId);
+        Task<TodoItem> GetByIdAsync(int id);
+        Task CreateAsync(TodoItem todoItem);
+        Task UpdateAsync(TodoItem todoItem);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ TodoListApp.Application/Interfaces/ITodoListService.cs
@@ -0,0 +1,13 @@
+﻿using TodoListApp.Domain.Entities;
+
+namespace TodoListApp.Application.Interfaces
+{
+    public interface ITodoListService
+    {
+        Task<IEnumerable<TodoList>> GetAllAsync();
+        Task<TodoList> GetByIdAsync(int id);
+        Task CreateAsync(TodoList todoList);
+        Task UpdateAsync(TodoList todoList);
+        Task DeleteAsync(int id);
+    }
+}
